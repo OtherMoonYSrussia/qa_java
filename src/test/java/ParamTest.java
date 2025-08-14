1caa43ce915877ac5fb11fa65262eca398f45442
OtherMoonYSrussia <redplace123@gmail.com>
2025-08-14 17:56:29 +0300

    Building mavern, added new test file

---
+++ src/test/java/ParamTest.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ParamTest {
+
+    @Mock
+    ...
+
+    @Spy
+    ...
+
+    @BeforeEach
+    ...
+
+    @Test
+    ...
+
+}
